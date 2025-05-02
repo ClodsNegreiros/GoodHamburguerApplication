178a6b68e2f0f5af93abdf51d882b95d98e49e14
ClodsNegreiros <clodsn@gmail.com>
2025-05-01 21:25:19 -0300

    feat: implemented Order, Sandwich and OrderMapping

---
+++ GoodHamburguerApplication.Infrastructure/Mappings/OrderMapping.cs
@@ -0,0 +1,30 @@
+﻿using GoodHamburguerApplication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GoodHamburguerApplication.Infrastructure.Mappings
+{
+    public class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(order => order.Id);
+
+            builder.Property(order => order.TotalPrice);
+            builder.Property(order => order.Discount);
+            builder.Property(order => order.CreatedAt);
+            builder.Property(order => order.SandwichId);
+            builder.Property(order => order.ExtraId);
+
+            builder
+              .HasOne(order => order.Sandwich)
+              .WithMany(sandwich => sandwich.Orders)
+              .HasForeignKey(order => order.SandwichId);
+
+            builder
+              .HasOne(order => order.Extra)
+              .WithMany(extra => extra.Orders)
+              .HasForeignKey(order => order.SandwichId);
+        }
+    }
+}
