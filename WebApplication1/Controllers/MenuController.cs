8e9057ad50add9487eedd2662b98af6ca69e0f82
ClodsNegreiros <clodsn@gmail.com>
2025-05-02 16:46:42 -0300

    feat: implemented MenuController to list sandwiches and extras

---
+++ WebApplication1/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using GoodHamburguerApplication.Application.Interfaces.Order;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodHamburgerApplication.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromServices] IGetSandwichesUseCase sandwichUseCase,
+            [FromServices] IGetExtrasUseCase extraUseCase)
+        {
+            var sandwichResult = await sandwichUseCase.Execute();
+            var extraResult = await extraUseCase.Execute();
+            
+            return Ok(new
+            {
+                Sandwiches = sandwichResult,
+                Extras = extraResult
+            });
+        }
+    }
+}
