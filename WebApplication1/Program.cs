4754609f49a27fe51ccea28348bc91d2e0ee9d88
ClodsNegreiros <clodsn@gmail.com>
2025-05-02 00:29:15 -0300

    feat: implemented seeder in the program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -51,6 +51,13 @@ namespace WebApplication1
             }
 
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                InitialDataSeeder.Seed(context);
+            }
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
