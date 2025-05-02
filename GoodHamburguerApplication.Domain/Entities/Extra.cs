997fc06cdab42b1442aab19c1e359538766664a4
ClodsNegreiros <clodsn@gmail.com>
2025-05-01 23:42:46 -0300

    fix: adjusted the type of Price property in Extra and Sandwich entity

--- GoodHamburguerApplication.Domain/Entities/Extra.cs
+++ GoodHamburguerApplication.Domain/Entities/Extra.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public List<Order> Orders { get; set; }
     }
 }
