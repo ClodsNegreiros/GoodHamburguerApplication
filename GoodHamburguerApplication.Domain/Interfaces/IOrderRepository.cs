026bf8ad644ee13dc7a7c93c8b501ed012f2f563
ClodsNegreiros <clodsn@gmail.com>
2025-05-01 21:54:47 -0300

    feat: implemented OrderRepository with Send, Get, Update and Delete methods

---
+++ GoodHamburguerApplication.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using GoodHamburguerApplication.Domain.Entities;
+
+namespace GoodHamburguerApplication.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public Task<Order> SendOrderAsync(Order order);
+        public Task<List<Order>> GetOrdersAsync();
+        public Task<Order> UpdateOrderAsync(Order order);
+        public Task<Order> DeleteOrderAsync(Order order);
+    }
+}
---
+++ GoodHamburguerApplication.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,46 @@
+﻿using GoodHamburguerApplication.Domain.Entities;
+using GoodHamburguerApplication.Domain.Interfaces;
+using GoodHamburguerApplication.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHamburguerApplication.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> SendOrderAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrdersAsync()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> UpdateOrderAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+
+            return order;
+        }
+
+        public async Task<Order> DeleteOrderAsync(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return order;
+        }
+    }
+}
